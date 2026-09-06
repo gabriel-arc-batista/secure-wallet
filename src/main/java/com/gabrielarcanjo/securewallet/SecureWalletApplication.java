277f03419017eb837bfd83df5bda1d53a172aeae
Gabriel <chessrex39@gmail.com>
2026-09-06 18:01:08 -0300

    chore: initialize Spring Boot Project

---
+++ src/main/java/com/gabrielarcanjo/securewallet/SecureWalletApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielarcanjo.securewallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielarcanjo/securewallet/SecureWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielarcanjo.securewallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

bfd2f0e691727d2c67cb1dcdfd7f1cf3971347d2
Gabriel Arcanjo <chessrex39@gmail.com>
2026-09-10 21:50:51 -0300

    feat: add user service

---
+++ src/main/java/com/gabrielarcanjo/securewallet/user/UserService.java
@@ -0,0 +1,13 @@
+package com.gabrielarcanjo.securewallet.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
